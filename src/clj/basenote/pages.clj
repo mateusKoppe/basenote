cba4d1a27160a8bd2f8d4a5e09d7e84856215f66
Mateus Koppe <mateus.koppe@gmail.com>
2021-06-27 18:04:01 -0300

    Implement database get-session

--- src/clj/basenote/pages.clj
+++ src/clj/basenote/pages.clj
@@ -1,40 +1,31 @@
 (ns clj.basenote.pages
   (:require [ring.util.response :refer [response]]
-            [codax.core :as codax]))
+            [codax.core :as codax]
+            [clj.basenote.database :refer [get-session]]))
 
 (defn create-page [page]
-  (let [db (codax/open-database! "data/basenote")]
-    (codax/assoc-at!
-     db
-     [:pages (:id page)]
-     {:id (:id page)
-      :title (:title page)})
-    (let [res-page (codax/get-at! db [:pages (:id page)])]
-      (codax/close-database! db)
-      res-page)))
+  (get-session
+   (fn [db]
+     (codax/assoc-at! db
+                      [:pages (:id page)]
+                      {:id (:id page)
+                       :title (:title page)})
+     (codax/get-at! db [:pages (:id page)]))))
 
 (defn get-pages []
-  (let [db (codax/open-database! "data/basenote")
-        list (codax/get-at! db [:pages])]
-    (codax/close-database! db)
-    list))
+  (get-session #(codax/get-at! % [:pages])))
 
-(defn update-page [page] 
-  (let [db (codax/open-database! "data/basenote")]
-    (codax/update-at!
-     db
-     [:pages (:id page)]
-     (fn 
-       [_] {:id (:id page)
-              :title (:title page)}))
-    (let [res-page (codax/get-at! db [:pages (:id page)])]
-      (codax/close-database! db)
-      res-page)))
+(defn update-page [page]
+  (get-session
+   (fn [db]
+     (codax/update-at! db
+                       [:pages (:id page)]
+                       (fn [_] {:id (:id page)
+                                :title (:title page)}))
+     (codax/get-at! db [:pages (:id page)]))))
 
 (defn remove-page [id]
-  (let [db (codax/open-database! "data/basenote")]
-    (codax/dissoc-at! db [:pages id])
-    (codax/close-database! db)))
+  (get-session #(codax/dissoc-at! % [:pages id])))
 
 ;; Routes
 
