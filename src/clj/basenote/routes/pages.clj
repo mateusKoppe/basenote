0181b36eeb4537ce6682edef5927fbcdb92bf22a
Mateus Koppe <mateus.koppe@gmail.com>
2021-07-06 22:01:39 -0300

    Handle errors in page

--- src/clj/basenote/routes/pages.clj
+++ src/clj/basenote/routes/pages.clj
@@ -10,50 +10,70 @@
     (merge base (action actions-rules))))
 
 (defn route-list [req]
-  (response (pages/get-pages)))
+  (try
+    (response (pages/get-pages))
+    (catch Exception _
+      (-> (response {:error "Error in the request"})
+          (status 500)))))
 
 (defn route-show [req]
-  (let [id (-> req :route-params :id)
-        page (pages/get-page id)]
-    (if (not page)
-      (-> (response nil)
-          (status 404))
-      (response page))))
+  (try
+    (let [id (-> req :route-params :id)
+          page (pages/get-page id)]
+      (if (not page)
+        (-> (response nil)
+            (status 404))
+        (response page)))
+    (catch Exception _
+      (-> (response {:error "Error in the request"})
+          (status 500)))))
 
 (defn route-create [req]
-  (let [exists (pages/page-exists? (-> req :body :id))
-        validation (st/validate (:body req) (page-schema :create))
-        errors (first validation)
-        fields (last validation)]
-    (if exists
-      (-> (response {:msg "id already used"})
-          (status 409))
-      (if errors
-        (-> (response errors)
-            (status 417))
-        (response (pages/create-page fields))))))
+  (try
+    (let [exists (pages/page-exists? (-> req :body :id))
+          validation (st/validate (:body req) (page-schema :create))
+          errors (first validation)
+          fields (last validation)]
+      (if exists
+        (-> (response {:msg "id already used"})
+            (status 409))
+        (if errors
+          (-> (response errors)
+              (status 417))
+          (response (pages/create-page fields)))))
+    (catch Exception _
+      (-> (response {:error "Error in the request"})
+          (status 500)))))
 
 (defn route-update [req]
-  (let [id (-> req :route-params :id)
-        exists (pages/page-exists? id)
-        validation (st/validate (:body req) (page-schema :update))
-        errors (first validation)
-        fields (last validation)]
-    (if (not exists)
-      (-> (response nil)
-          (status 404))
-      (if errors
-        (-> (response errors)
-            (status 417))
-        (response
-         (pages/update-page (merge fields {:id id})))))))
+  (try
+    (let [id (-> req :route-params :id)
+          exists (pages/page-exists? id)
+          validation (st/validate (:body req) (page-schema :update))
+          errors (first validation)
+          fields (last validation)]
+      (if (not exists)
+        (-> (response nil)
+            (status 404))
+        (if errors
+          (-> (response errors)
+              (status 417))
+          (response
+           (pages/update-page (merge fields {:id id}))))))
+    (catch Exception _
+      (-> (response {:error "Error in the request"})
+          (status 500)))))
 
 (defn route-delete [req]
-  (let [id (-> req :route-params :id)
-        exists (pages/page-exists? id)]
-    (if (not exists)
-      (-> (response nil)
-          (status 404))
-      (do
-        (pages/remove-page id)
-        (response nil)))))
+  (try
+    (let [id (-> req :route-params :id)
+          exists (pages/page-exists? id)]
+      (if (not exists)
+        (-> (response nil)
+            (status 404))
+        (do
+          (pages/remove-page id)
+          (response nil))))
+    (catch Exception _
+      (-> (response {:error "Error in the request"})
+          (status 500)))))
