32988c95041265055314bf9c372d13ca3ebb9a4d
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2021-08-03 20:42:10 -0300

    Revert "chore(frontend): Tailwindcss setup"

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-const App = () => {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
     <div className="App">
-      <header className="App-header p-2">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
