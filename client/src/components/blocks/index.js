6a9cb9306c6bd92d7acd72e9e17a454bb22ea7a3
Kellane <kellanelopes2000@gmail.com>
2021-08-31 22:23:21 -0300

    Implement block title

--- client/src/components/blocks/index.js
+++ client/src/components/blocks/index.js
@@ -1,5 +1,7 @@
 import TextBlock from "./TextBlock";
+import TitleBlock from "./TitleBlock";
 
 export const blockTypes = {
-  text: { label: "Text", render: TextBlock }
+  text: { label: "Text", render: TextBlock },
+  title: { label: "Title", render: TitleBlock }
 };
