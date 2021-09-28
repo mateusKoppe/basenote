402b92bc0debbb47392437620ab68310b8907a34
Kellane Pereira <kellane.pereira@animati.com.br>
2021-09-28 17:59:11 -0300

    CRUD pages

---
+++ client/src/components/pages/AddPages.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button, Form, InputGroup, FormControl } from "react-bootstrap";
+
+
+const AddPages = ({ onSave = () => { } }) => {
+  const [isCreating, setIsCreating] = useState(false);
+  const [pageTitle, setPageTitle] = useState();
+
+  const handleChange = (event) => {
+    setPageTitle(event.target.value);
+  };
+
+  const handleSave = async () => {
+    await onSave({ title: pageTitle });
+    setIsCreating(false);
+    setPageTitle();
+  };
+
+  return isCreating ? (
+    <Form>
+       <InputGroup className="mb-3">
+        <InputGroup.Text>Titulo</InputGroup.Text>
+        <FormControl
+          onChange={handleChange}
+          aria-label="Title"
+        />
+      </InputGroup>
+      <Button onClick={handleSave}>Save</Button>
+    </Form>
+  ) : (
+    <Button onClick={() => setIsCreating(true)}>Add</Button>
+  );
+};
+
+export default AddPages;
--- client/src/pages/Blocks.jsx
+++ client/src/pages/Blocks.jsx
@@ -1,18 +1,45 @@
 import useBlocks from "../hooks/useBlocks";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { blockTypes } from "../components/blocks";
 import AddBlock from "../components/blocks/AddBlock";
-import { Button } from "react-bootstrap";
+import { Button, Form, InputGroup, FormControl } from "react-bootstrap";
+import usePages from "../hooks/usePages";
 
 const Blocks = () => {
   const { pageId } = useParams();
-
+  const [isEditing, setIsEditing] = useState(false);
   const { blocks, addBlock, updateBlock, deleteBlock } = useBlocks(pageId);
+  const { getPage, updatePage } = usePages();
+
+  const page = getPage(pageId);
+  const [pageTitle, setPageTitle] = useState();
+  
+  const handleChange = (event) => {
+    setPageTitle(event.target.value);
+  };
+
+  const handleSave = () => {
+    updatePage(page.id, {title: pageTitle})
+    setIsEditing(false)
+  }
 
   return (
     <div>
-      <h2>Page</h2>
+      {isEditing
+      ? <Form>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Titulo</InputGroup.Text>
+            <FormControl
+              onChange={handleChange}
+              aria-label="Title"
+              />
+          </InputGroup>
+          <Button onClick={() => handleSave() }>Save</Button>
+        </Form>
+      : <h2 onDoubleClick={() => setIsEditing(true)}>{page?.title}</h2>}
+
       {blocks.map((block) => {
         const Block = blockTypes[block.type].render;
         return (
