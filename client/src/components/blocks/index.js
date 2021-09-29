7edba0c7d581adc9b253355181e53c25b068e2aa
arufonsekun <junior.ramisch@gmail.com>
2021-09-28 22:28:23 -0300

    feat(TodoList): Adds todo list block
    UI improvements needed

--- client/src/components/blocks/index.js
+++ client/src/components/blocks/index.js
@@ -1,5 +1,7 @@
 import TextBlock from "./TextBlock";
+import TodoBlock from "./TodoBlock/index";
 
 export const blockTypes = {
-  text: { label: "Text", render: TextBlock }
+  text: { label: "Text", render: TextBlock },
+  todoList: { label: "TodoList", render: TodoBlock },
 };
