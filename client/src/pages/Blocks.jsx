fe204499ac2cf9c67516cb50d88df80ece27e70d
Mateus Koppe <mateus.koppe@gmail.com>
2021-09-28 22:17:22 -0300

    Implement block editing mode

--- client/src/pages/Blocks.jsx
+++ client/src/pages/Blocks.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import useBlocks from "../hooks/useBlocks";
 import { useParams } from "react-router-dom";
 
 import { blockTypes } from "../components/blocks";
 import AddBlock from "../components/blocks/AddBlock";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 const Blocks = () => {
   const { pageId } = useParams();
 
   const { blocks, addBlock, updateBlock, deleteBlock } = useBlocks(pageId);
+  const [editingBlock, setEditingBlock] = useState();
 
   return (
     <div>
@@ -19,13 +21,22 @@ const Blocks = () => {
           <div key={block.id}>
             <Block
               value={block.data}
+              isEditing={block.id === editingBlock?.id}
+              onEdit={() => setEditingBlock(block)}
               onChange={(data) => {
                 updateBlock(block.id, data);
               }}
             />
-            <Button size="sm" variant="danger" onClick={() => deleteBlock(block.id)}>
-              Delete
-            </Button>
+            { block.id === editingBlock?.id && (
+              <ButtonGroup className="mb-2" size="sm">
+                <Button variant="outline-danger" onClick={() => deleteBlock(block.id)}>
+                  Delete
+                </Button>
+                <Button variant="outline-secondary" onClick={() => setEditingBlock(null)}>
+                  Close
+                </Button>
+              </ButtonGroup>
+            ) }
           </div>
         );
       })}
